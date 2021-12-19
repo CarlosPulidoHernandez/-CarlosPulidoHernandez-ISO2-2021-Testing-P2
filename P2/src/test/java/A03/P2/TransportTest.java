5dfefca2e74ac5a70ff3863b0177bddd2be22618
Alberto Quintana <91602621+Albertill0@users.noreply.github.com>
2021-12-19 13:27:10 +0100

    New test cases
    Added new test cases to the set

--- P2/src/test/java/A03/P2/TransportTest.java
+++ P2/src/test/java/A03/P2/TransportTest.java
@@ -9,12 +9,30 @@ public class TransportTest {
 	// Decision coverage Tests
 	
 	@Test
+	public void testLevel0Age6() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(50,10);
+		Person p = new Person(6,true,true,true);
+		assertTrue(t.getTicket(p)==10*0.4);
+	}
+	
+	@Test
 	public void testLevel0Age22() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
 		Transport t = new Transport(50,10);
 		Person p = new Person(22,false,true,false);
 		assertTrue(t.getTicket(p)==10*0.4);
 	}
-	
+	@Test
+	public void testLevel0Age23() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(50,10);
+		Person p = new Person(23,false,false,false);
+		assertTrue(t.getTicket(p)==10);
+	}
+	@Test
+	public void testLevel0Age30() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(50,10);
+		Person p = new Person(30,false,true,false);
+		assertTrue(t.getTicket(p)==10);
+	}
 	@Test
 	public void testLevel0Age65() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
 		Transport t = new Transport(50,10);
@@ -28,6 +46,18 @@ public class TransportTest {
 		Person p = new Person(66,false,true,false);
 		assertTrue(t.getTicket(p)==10*0.2);
 	}
+	@Test
+	public void testLevel0Age70() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(50,10);
+		Person p = new Person(70,true,false,true);
+		assertTrue(t.getTicket(p)==10*0.2);
+	}
+	@Test
+	public void testLevel1Age6() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(150,10);
+		Person p = new Person(6,true,true,true);
+		assertTrue(t.getTicket(p)==10*0.7);
+	}
 	
 	@Test
 	public void testLevel1Age22() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
@@ -35,7 +65,18 @@ public class TransportTest {
 		Person p = new Person(22,false,true,false);
 		assertTrue(t.getTicket(p)==10*0.7);
 	}
-	
+	@Test
+	public void testLevel1Age23() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(150,10);
+		Person p = new Person(23,false,false,false);
+		assertTrue(t.getTicket(p)==10);
+	}
+	@Test
+	public void testLevel1Age30() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(150,10);
+		Person p = new Person(30,false,true,false);
+		assertTrue(t.getTicket(p)==10);
+	}
 	@Test
 	public void testLevel1Age65() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
 		Transport t = new Transport(150,10);
@@ -48,6 +89,24 @@ public class TransportTest {
 		Transport t = new Transport(150,10);
 		Person p = new Person(66,false,true,false);
 		assertTrue(t.getTicket(p)==10*0.5);
+	}
+	@Test
+	public void testLevel1Age70() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(150,10);
+		Person p = new Person(70,true,false,true);
+		assertTrue(t.getTicket(p)==10*0.5);
+	}
+	@Test
+	public void testLevel2Age22() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(250,10);
+		Person p = new Person(22,false,true,false);
+		assertTrue(t.getTicket(p)==10*0.8);
+	}
+	@Test
+	public void testLevel2Age30() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(250,10);
+		Person p = new Person(30,false,true,false);
+		assertTrue(t.getTicket(p)==10);
 	}
 	
 	@Test
@@ -63,12 +122,29 @@ public class TransportTest {
 		Person p = new Person(66,false,true,false);
 		assertTrue(t.getTicket(p)==10*1.2);
 	}
-	
+	@Test
+	public void testLevel2Age70() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(250,10);
+		Person p = new Person(70,true,false,true);
+		assertTrue(t.getTicket(p)==10*1.2);
+	}
+	@Test
+	public void testLevel3Age6() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(450,10);
+		Person p = new Person(6,true,true,true);
+		assertTrue(t.getTicket(p)==10*1.2);
+	}
 	@Test
 	public void testLevel3Age22() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
 		Transport t = new Transport(450,10);
 		Person p = new Person(22,false,true,false);
 		assertTrue(t.getTicket(p)==10*1.2);
+	}
+	@Test
+	public void testLevel3Age30() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(450,10);
+		Person p = new Person(30,false,true,false);
+		assertTrue(t.getTicket(p)==10);
 	}
 	
 	@Test
@@ -82,6 +158,12 @@ public class TransportTest {
 	public void testLevel3Age66() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
 		Transport t = new Transport(450,10);
 		Person p = new Person(66,false,true,false);
+		assertTrue(t.getTicket(p)==10*1.5);
+	}
+	@Test
+	public void testLevel3Age70() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
+		Transport t = new Transport(450,10);
+		Person p = new Person(70,true,false,true);
 		assertTrue(t.getTicket(p)==10*1.5);
 	}
 	
