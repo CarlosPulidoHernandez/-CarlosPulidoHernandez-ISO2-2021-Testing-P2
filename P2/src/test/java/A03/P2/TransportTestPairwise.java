4351850ac5cd279df3288e80e1bd6d1eaa0dcbe9
Alberto Quintana <91602621+Albertill0@users.noreply.github.com>
2021-12-20 20:12:14 +0100

    Add files via upload

--- P2/src/test/java/A03/P2/TransportTestPairwise.java
+++ P2/src/test/java/A03/P2/TransportTestPairwise.java
@@ -8,26 +8,26 @@ public class TransportTestPairwise {
 	@Test
 	public void Pairwise1level0() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
 		Transport t = new Transport(50,10);
-		Person p = new Person(22,true,false,true);
+		Person p = new Person(22,true,true,false);
 		assertTrue(t.getTicket(p)==10*0.4);
 	}
 	@Test
 	public void Pairwise2level1() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
 		Transport t = new Transport(150,10);
-		Person p = new Person(23,false,false,false);
+		Person p = new Person(23,false,true,false);
 		assertTrue(t.getTicket(p)==10);
 	}
 	@Test
 	public void Pairwise3level2() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
 		Transport t = new Transport(250,10);
-		Person p = new Person(64,false,true,true);
+		Person p = new Person(64,false,true,false);
 		assertTrue(t.getTicket(p)==10);
 	}
 	@Test
 	public void Pairwise4level3() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
 		Transport t = new Transport(450,10);
-		Person p = new Person(65,false,false,false);
-		assertTrue(t.getTicket(p)==10*1.5);
+		Person p = new Person(65,false,true,false);
+		assertTrue(t.getTicket(p)==10);
 	}
 	@Test
 	public void Pairwise5level4() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
@@ -39,20 +39,20 @@ public class TransportTestPairwise {
 		t.getTicket(p);
 		}catch(NoSeatsAvailableException e) {
 			ThrowExcept = true;
-			assert(ThrowExcept);
+			assertTrue(ThrowExcept);
 		}
 		}
 	@Test
 	public void Pairwise6level4() throws NegativeValueException, NoSeatsAvailableException, NotHealthyException {
 		Transport t = new Transport(550,10);
-		Person p = new Person(2147483646,true,false,true);
+		Person p = new Person(2147483646,true,true,false);
 		//Expected customized NoSeatsAvaliable exception
 		boolean ThrowExcept = false;
 		try {
 		t.getTicket(p);
 		}catch(NoSeatsAvailableException e) {
 			ThrowExcept = true;
-			assert(ThrowExcept);
+			assertTrue(ThrowExcept);
 		}
 		}
 }
