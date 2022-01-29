08f9593f895da1bc3fd1b88373b3b8f95ffea76b
CarlosPulidoHernandez <Carlos.Pulido@alu.uclm.es>
2022-01-29 12:56:41 +0100

    Revert "Added Maintenance Changes"
    This reverts commit fc15495f616c561a8f95f0a1b1162b8d5d595094.

--- P2/src/main/java/A03/P2/NotAllowedValueException.java
+++ P2/src/main/java/A03/P2/NotAllowedValueException.java
@@ -1,7 +1,7 @@
 package A03.P2;
 
 public class NotAllowedValueException extends Exception{
-	public NotAllowedValueException (final String errorMessage) {
+	public NotAllowedValueException (String errorMessage) {
 		super(errorMessage);
 	}
 }
